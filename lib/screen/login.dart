3d4c088bfc51dffc9dfafe6a419ff6086e15a49f
huy <116376784+Huykhoai@users.noreply.github.com>
2024-11-13 12:07:35 +0700

    login.dart

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -75,190 +75,190 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          padding: const EdgeInsets.only(left: 0,top: 30.0,bottom: 0,right: 0),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin:Alignment.topCenter,
-              colors:[
-                Colors.blue,
-                Colors.blue[400]!,
-                Colors.blue[200]!
-              ]
-          )
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40,),
-            const Padding(
-              padding:EdgeInsets.all(20),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FadeAnimation(1, Text("Login", style: TextStyle(color: Colors.white,fontSize: 40),)),
-                    SizedBox(height: 10,),
-                    FadeAnimation(1.3,Text("Welcome Back", style: TextStyle(color: Colors.white,fontSize: 18),))
-                  ]
+      body: SingleChildScrollView(
+        child: Container(
+            padding: const EdgeInsets.only(left: 0,top: 30.0,bottom: 0,right: 0),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin:Alignment.topCenter,
+                colors:[
+                  Colors.blue,
+                  Colors.blue[400]!,
+                  Colors.blue[200]!
+                ]
+            )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40,),
+              const Padding(
+                padding:EdgeInsets.all(20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FadeAnimation(1, Text("Login", style: TextStyle(color: Colors.white,fontSize: 40),)),
+                      SizedBox(height: 10,),
+                      FadeAnimation(1.3,Text("Welcome Back", style: TextStyle(color: Colors.white,fontSize: 18),))
+                    ]
+                ),
               ),
-            ),
-            const SizedBox(height:20,),
-            Expanded(
-                child: Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(60),topRight: Radius.circular(60))
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 60,),
-                        FadeAnimation(1.4, Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow:  [
-                                BoxShadow(
-                                    color: Colors.blue[200]!,
-                                    blurRadius: 20,
-                                    offset: const Offset(0, 10)
-                                )]
-                          ),
-                          padding: const EdgeInsets.all(20),
-                          child:Column(
-                            children: [
-                              Container(
-                                padding:const EdgeInsets.all(10),
-                                decoration: const BoxDecoration(
-                                    border: Border(bottom: BorderSide(color: Color.fromARGB(0, 238, 238, 238)))
-                                ),
-                                child: TextField(
-                                  controller: _emailController,
-                                  decoration: InputDecoration(
-                                      hintText: "Email or Phone number",
-                                      hintStyle: const TextStyle(color: Colors.grey),
-                                      border: InputBorder.none,
-                                      errorText: emailError,
-                                      errorBorder:emailError.isNotEmpty
-                                          ? const OutlineInputBorder(borderSide: BorderSide(color: Colors.red))
-                                          : null,
-                                      icon: const Icon(Icons.email),
-
-                                  ),
-                                ),
-                              )
-                              ,
-                              Container(
-                                padding:const EdgeInsets.all(10),
-                                decoration: const BoxDecoration(
-                                    border: Border(bottom: BorderSide(color: Color.fromARGB(0, 238, 238, 238)))
-                                ),
-                                child:  TextField(
-                                  controller: _passwordController,
-                                  obscureText: _obscureText ? true : false,
-                                  decoration: InputDecoration(
-                                    hintText: "Password",
+              const SizedBox(height:20,),
+              Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60),topRight: Radius.circular(60))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 60,),
+                      FadeAnimation(1.4, Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow:  [
+                              BoxShadow(
+                                  color: Colors.blue[200]!,
+                                  blurRadius: 20,
+                                  offset: const Offset(0, 10)
+                              )]
+                        ),
+                        padding: const EdgeInsets.all(20),
+                        child:Column(
+                          children: [
+                            Container(
+                              padding:const EdgeInsets.all(10),
+                              decoration: const BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Color.fromARGB(0, 238, 238, 238)))
+                              ),
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                    hintText: "Email or Phone number",
                                     hintStyle: const TextStyle(color: Colors.grey),
                                     border: InputBorder.none,
-                                    errorText: passwordError,
-                                    errorBorder:passwordError.isNotEmpty
+                                    errorText: emailError,
+                                    errorBorder:emailError.isNotEmpty
                                         ? const OutlineInputBorder(borderSide: BorderSide(color: Colors.red))
                                         : null,
-                                    icon: const Icon(Icons.key),
-                                    suffixIcon: IconButton(
-                                        onPressed: _togglePasswordView,
-                                        icon: Icon(
-                                          _obscureText ? Icons.visibility_off : Icons.visibility
-                                        ),
-                                    ),
-                                    suffixIconColor: Colors.black
-                                  ),
+                                    icon: const Icon(Icons.email),
+
                                 ),
-                              )
-                            ],
-                          ),
-                        )
-                        ),
-                        //Text(error, style: const TextStyle(color: Colors.red),),
-                        const SizedBox(height: 40,),
-                        const FadeAnimation(1.5,Text("Forgot Password? ", style: TextStyle(color: Colors.grey),)),
-                        const SizedBox(height: 20,),
-                        GestureDetector(
-                          child: FadeAnimation(1.6, Container(
-                            height: 50,
-                            margin: const EdgeInsets.symmetric(horizontal: 50),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: const Color(0xFFFF5722),
-                            ),
-                            child: Center(
-                              child: isLoading
-                              ? const CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation(Colors.white),)
-                              : const Text(
-                                "Login",
-                                style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
-                            ),
-                          )
-                          ),
-                          onTap: (){
-                            login();
-                          },
-                        ),
-                        const SizedBox(height: 20,),
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            FadeAnimation(1.7, Text("Don't have an account yet? ",style: TextStyle(color: Colors.grey),)),
-                            FadeAnimation(1.7, Text("Register ",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),))
-                          ],
-                        ),
-                        const SizedBox(height: 10,),
-                        const FadeAnimation(1.75, Text("Or")),
-                        const SizedBox(height: 10,),
-                        const FadeAnimation(1.8, Text("Continue with social media",style: TextStyle(color: Colors.grey),)),
-                        const SizedBox(height: 30,),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: FadeAnimation(1.9, Container(
-                                height:50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: Colors.blue
-                                ),
-                                child: const Center(
-                                  child: Text("Facebook", style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
-                                ),
-                              )
                               ),
-                            ),
-                            const SizedBox(width: 30,),
-                            Expanded(
-                              child: FadeAnimation(1.9,Container(
-                                height:50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: Colors.black
+                            )
+                            ,
+                            Container(
+                              padding:const EdgeInsets.all(10),
+                              decoration: const BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Color.fromARGB(0, 238, 238, 238)))
+                              ),
+                              child:  TextField(
+                                controller: _passwordController,
+                                obscureText: _obscureText ? true : false,
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: const TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                  errorText: passwordError,
+                                  errorBorder:passwordError.isNotEmpty
+                                      ? const OutlineInputBorder(borderSide: BorderSide(color: Colors.red))
+                                      : null,
+                                  icon: const Icon(Icons.key),
+                                  suffixIcon: IconButton(
+                                      onPressed: _togglePasswordView,
+                                      icon: Icon(
+                                        _obscureText ? Icons.visibility_off : Icons.visibility
+                                      ),
+                                  ),
+                                  suffixIconColor: Colors.black
                                 ),
-                                child: const Center(
-                                  child: Text("Github", style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
-                                ),
-                              )
                               ),
                             )
                           ],
+                        ),
+                      )
+                      ),
+                      //Text(error, style: const TextStyle(color: Colors.red),),
+                      const SizedBox(height: 40,),
+                      const FadeAnimation(1.5,Text("Forgot Password? ", style: TextStyle(color: Colors.grey),)),
+                      const SizedBox(height: 20,),
+                      GestureDetector(
+                        child: FadeAnimation(1.6, Container(
+                          height: 50,
+                          margin: const EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: const Color(0xFFFF5722),
+                          ),
+                          child: Center(
+                            child: isLoading
+                            ? const CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(Colors.white),)
+                            : const Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
+                          ),
                         )
-                      ],
-                    ),
+                        ),
+                        onTap: (){
+                          login();
+                        },
+                      ),
+                      const SizedBox(height: 20,),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FadeAnimation(1.7, Text("Don't have an account yet? ",style: TextStyle(color: Colors.grey),)),
+                          FadeAnimation(1.7, Text("Register ",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),))
+                        ],
+                      ),
+                      const SizedBox(height: 10,),
+                      const FadeAnimation(1.75, Text("Or")),
+                      const SizedBox(height: 10,),
+                      const FadeAnimation(1.8, Text("Continue with social media",style: TextStyle(color: Colors.grey),)),
+                      const SizedBox(height: 30,),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: FadeAnimation(1.9, Container(
+                              height:50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.blue
+                              ),
+                              child: const Center(
+                                child: Text("Facebook", style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
+                              ),
+                            )
+                            ),
+                          ),
+                          const SizedBox(width: 30,),
+                          Expanded(
+                            child: FadeAnimation(1.9,Container(
+                              height:50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.black
+                              ),
+                              child: const Center(
+                                child: Text("Github", style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.bold),),
+                              ),
+                            )
+                            ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                )
+                ),
+              )
+            ],
             )
-          ],
-    )
-    )
+            ),
+      )
     );
   }
 }
